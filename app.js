72e6bb786e9916939fcf6b28f4b2474a51be39a4
Shahbaz Kamil <72928046+shahbazthedev@users.noreply.github.com>
2021-01-07 06:03:36 -0800

    Update app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+AOS.init({
+    offset:200,
+    delay:100,
+    duration:1000,
+});
+
+const scrolltp = document.querySelector("#scrolltp");
+scrolltp.addEventListener("click", function(){
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth",
+    });
+});
+
+window.addEventListener ("scroll" , function (){
+    if(window.scrollY >= 700) {
+        scrolltp.style.opacity =1;
+    }else {
+        scrolltp.style.opacity = 0;
+    }
+})
