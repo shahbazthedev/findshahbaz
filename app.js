6e2d1797cd548deaa8553775ba1b2d5002652153
Shahbaz Kamil <72928046+shahbazthedev@users.noreply.github.com>
2021-02-25 03:13:54 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 AOS.init({
     offset:200,
     delay:100,
@@ -22,8 +23,44 @@ window.addEventListener ("scroll" , function (){
 })
 
 
-function dark (){
-    window.alert("Submitted");
-}
+const name = document.getElementById('name');
+const myForm = document.getElementById('myForm');
+const yourBudget = document.getElementById('budget');
+const project = document.getElementById('description');
+const email = document.getElementById('email');
+const errorElemt = document.getElementById('error');
+
+
+myForm.addEventListener('submit', (e) => {
+ 
+   if (name.value === '' || name.value == null){
+       alert('Name is required');
+   }
+
+   if(yourBudget.value < 2000){
+      alert('Budget must be greater then 1k')
+   }
+
+   if(project.value == null || project.value ==='' || project.value.length < 10){
+    alert('Please explain your project');
+   }
+
+   if (message.length > 0){
+    e.preventDefault();
+
+    
+   const request = new XMLHttpRequest();
+   request.open("post" , "login.php");
+   request.onload = function(){
+       console.log(request.responseText);
+   }
+
+   request.send(new FormData (myForm));
+    errorElemt.innerText = message.join(' ,');
+   }
+    
+})
+
+
 
 
