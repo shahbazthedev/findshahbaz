35f6f1f5e19f5a3bdd1076a0947e0b736f495538
Shahbaz Kamil <72928046+shahbazthedev@users.noreply.github.com>
2021-02-28 07:20:36 -0800

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ window.addEventListener ("scroll" , function (){
 })
 
 
-const name = document.getElementById('name');
+/*const name = document.getElementById('name');
 const myForm = document.getElementById('myForm');
 const yourBudget = document.getElementById('budget');
 const project = document.getElementById('description');
@@ -60,7 +60,56 @@ myForm.addEventListener('submit', (e) => {
    }
     
 })
+*/
 
+
+  window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("myForm");
+    var status = document.getElementById("status");
+
+
+    // Success and Error functions for after the form is submitted
+
+    
+    function success() {
+      form.reset();
+      status.classList.add('success');
+      status.innerHTML = "Your submission has been sent.";
+    }
+
+    function error() {
+        status.classList.add('error');
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
 
 
 
